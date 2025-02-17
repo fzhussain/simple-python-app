752eb8edaf3981a84a7e5edc1132c532f124ea48
Faraz <farazhussain503@gmail.com>
2025-02-17 12:39:26 +0530

    update files for code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo "Puliing the docker image from Docker Hub"
 docker pull fzhussain/simple-python-app
 
 # Run the Docker image as a container -d: daemon process
-echo "Running the Docker Image"
 docker run -d -p 5000:5000 fzhussain/simple-python-app
