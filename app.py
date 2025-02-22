f0c14f2566b15beecfc96658285e6507f469fec5
Faraz <farazhussain503@gmail.com>
2025-02-22 16:04:48 +0530

    To validate CICD

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!!!!!!!!!!'
+    return 'Hello, Faraz. I am the final change to validate CICD!!!!!!!!!!'
 
 if __name__ == '__main__':
     app.run()
